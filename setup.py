dbe8384b016c8a508430cfdceaf6715faf15b104
Charles Smith <charsmith@netflix.com>
2014-05-22 19:34:43 -0700

    fix some setup

--- setup.py
+++ setup.py
@@ -6,9 +6,8 @@ setup(
     version = '0.1',
     description = 'A library for easy configuration',
     author = 'Charles Smith, Jeff Magnusson',
-    author_email = 'charsmith@gmail.com, magnussj@gmail.com',
+    author_email = 'charles.s.smith@gmail.com, magnussj@gmail.com',
     url = 'https://github.com/charsmith/configurator', # use the URL to the github repo
-    download_url = 'https://github.com/charmsith/configurator/tarball/0.1', # I'll explain this in a second
     keywords = ['configuration', 'ini'], # arbitrary keywords
     license = 'Apache Software License',
     classifiers = [
